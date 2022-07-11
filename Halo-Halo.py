3989d2511a54cfab01f07f412e19cfd2b53b0f36
Alfiyanto Kondolele <alfiyanto.kondolele@gmail.com>
2022-07-12 07:32:57 +0800

    Replace aku -> saya

--- Halo-Halo.py
+++ Halo-Halo.py
@@ -10,10 +10,10 @@ st.sidebar.markdown("# Halo-Halo 😄")
 st.markdown(
     """
     <h1>Halo-Halo 😄</h1>
-    <p>Perkenalkan namaku Apping. Sekarang sedang berkuliah di salah satu universitas di Bandung.
-    Untuk lebih lengkapnya kalian bisa lihat profil aku di <a href='https://haloapping.github.io/' target='blank'>haloapping</a>.</p>
-    <p>Pada webiste ini aku akan <i>#sharingtipistipis</i> catatan selama belajar mengenai <i>machine learning</i>, <i>deep learning</i> dan masih banyak lagi.
+    <p>Perkenalkan nama saya Apping. Sekarang sedang berkuliah di salah satu kampus di Bandung.
+    Untuk lebih lengkapnya kalian bisa lihat profil saya di <a href='https://haloapping.github.io/' target='blank'>haloapping</a>.</p>
+    <p>Pada webiste ini saya akan <i>#sharingtipistipis</i> catatan selama belajar mengenai <i>machine learning</i>, <i>deep learning</i> dan masih banyak lagi.
     Bahasannya akan seputar hal-hal yang sifatnya simpel dan fundamental.</p>
-    <p>Aku harap semoga catatan simpelku ini bisa bermanfaat buat teman-teman sekalian. Semangat belajar!</p>""",
+    <p>Saya berharap semoga catatan simpel saya ini bisa bermanfaat buat teman-teman sekalian. Semangat belajar!</p>""",
     unsafe_allow_html=True
 )
